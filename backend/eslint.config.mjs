f9d179dea1336c402cfbf7b1f5a2fdf717744230
Gustavo123350 <gutafava@outlook.com>
2024-04-29 23:12:28 -0300

    Projeto: P2

---
+++ backend/eslint.config.mjs
@@ -0,0 +1,47 @@
+import { browser, node } from "globals";
+import { configs } from "typescript-eslint";
+
+export default {
+  env: {
+    es2021: true,
+    node: true
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module"
+  },
+  plugins: [
+    "@typescript-eslint"
+  ],
+  rules: {
+    indent: [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    quotes: [
+      "error",
+      "single"
+    ],
+    semi: [
+      "error",
+      "always"
+    ],
+    "@typescript-eslint/no-empty-interface": "off"
+  },
+  languageOptions: {
+    globals: {
+      ...browser,
+      ...node
+    }
+  },
+  ...configs.recommended
+};
